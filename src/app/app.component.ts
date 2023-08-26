0688b74579fbf5c74b5685d9597699128f5fe39b
alonemanuel <alon@alonemanuel.com>
2023-08-26 16:12:41 +0300

    Frontend now has list of books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+// Import Angular core libraries
 import { Component } from '@angular/core';
 
+// Component decorator to define metadata
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: `app-root`, // HTML tag where this component will be inserted
+  templateUrl: `./app.component.html`, // Path to the HTML template
+  styleUrls: [`./app.component.css`], // Path to the CSS styles
 })
 export class AppComponent {
-  title = 'your-project-name';
+  title = `Book List`; // Title of the application
+  bookName: string = ``; // Variable to hold the name of the book to be added
+  books: string[] = []; // Array to hold the list of books
+
+  // Method to add a book to the list
+  addBook() {
+    // Check if the bookName is not empty
+    if (this.bookName) {
+      this.books.push(this.bookName); // Add the book to the books array
+      this.bookName = ``; // Clear the input field
+    }
+  }
 }
